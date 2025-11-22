4f8a73b87a941a65ed8e7d074ecbae7621ff5df8
jamesw1892 <gingersemaj1892@gmail.com>
2025-11-22 19:38:25 +0000

    Get lines from API

--- tfls/__init__.py
+++ tfls/__init__.py
@@ -1,11 +1,5 @@
-"""
-From https://github.com/dhilmathy/TfL-python-api
-"""
-
-
 from .core import *
 from .custom_api import *
 from .station import *
 from .line import *
 from .train import *
-
--- tfls/core.py
+++ tfls/core.py
@@ -1,21 +1,30 @@
-import os
+from tfls.custom_api import tfl_get
+
+
+MODES = [
+    #'bus',
+    #'cable-car',
+    #'coach',
+    #'cycle',
+    #'cycle-hire',
+    #'dlr',
+    'elizabeth-line',
+    #'interchange-keep-sitting',
+    #'interchange-secure',
+    #'national-rail',
+    #'overground',
+    #'replacement-bus',
+    #'river-bus',
+    #'river-tour',
+    #'taxi',
+    #'tram',
+    'tube',
+    #'walking'
+]
 
 def verify():
     return 'tfls is working!'
 
 
 def get_line_ids():
-    return [
-        'victoria',
-        'central',
-        'northern',
-        'bakerloo',
-        'piccadilly',
-        'dlr',
-        'metropolitan',
-        'jubilee',
-        'circle',
-        'district'
-        # 'hammersmith & city'
-        # 'waterloo & city',
-    ]
+    return [line["name"] for mode in MODES for line in tfl_get(f"/Line/Mode/{mode}")]
